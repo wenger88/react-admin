4ab174fad2ed07ee62330ea7ec7af54d6c898b73
Goran <pavlovskigoran88@gmail.com>
2021-03-28 20:57:58 +0200

    register page and form

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -10,7 +10,6 @@ class Wrapper extends Component {
         <div className="container-fluid">
           <div className="row">
             <Menu />
-
             <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">{this.props.children}</main>
           </div>
         </div>
